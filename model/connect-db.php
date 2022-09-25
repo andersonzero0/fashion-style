7473ba7f2267fd5b65e4ca2d3220fb7536949ea2
anderson <92057959+andersonzero0@users.noreply.github.com>
2022-09-25 10:24:00 -0300

    update

---
+++ model/connect-db.php
@@ -0,0 +1,14 @@
+<?php
+#connect
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "fashionstyle";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+?>
