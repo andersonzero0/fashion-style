609c2945d42ee6d6cc465c0b420f6a4be39fec90
anderson <92057959+andersonzero0@users.noreply.github.com>
2022-09-25 14:56:00 -0300

    update

--- controller/cadastro.php
+++ controller/cadastro.php
@@ -4,5 +4,12 @@ require "../model/connect-db.php";
 $usuario_cad = $_POST['usuario_cad'];
 $senha_cad = $_POST['senha_cad'];
 
- 
+$sql = "INSERT INTO usuarios (usuario, senha)
+VALUES ('$usuario_cad', '$senha_cad')";
+
+if (mysqli_query($conn, $sql)) {
+    header("location: ../view/login.php");
+  } else {
+    header("location: ../view/cadastrar.php");
+  }
 ?>
