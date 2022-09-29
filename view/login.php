b799b1de60bc097917fc2d8087df128a91844058
gabriel da silva pires <mr.boostphoenix@gmail.com>
2022-09-29 19:26:32 -0300

    Atualização dos Arquivos

--- view/login.php
+++ view/login.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/login.css" type="text/css">
     <title>Login - Fashion Style</title>
 </head>
 <body>
