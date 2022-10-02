2ad74131f978973c925f9b518bb40d3a227ba7a3
gabriel da silva pires <mr.boostphoenix@gmail.com>
2022-10-02 00:21:36 -0300

    Atualização de Estilização!
    Esse commit é relativo às atualizações do projeto, mais especificamente sobre o formulário de cadastro.

--- view/cadastrar.php
+++ view/cadastrar.php
@@ -24,23 +24,22 @@
         <div id="cadastro-box">
             <form action="../controller/cadastro.php" method="post" id="form-cadastro">
                 <fieldset class="campo">
+                    <legend>Indentificação do Usuário</legend>
                     <label for="usuario_cad" id="etiqueta1">USUÁRIO:</label>
-                    <input type="text" name="usuario_cad" id="usuario_cad" size="60" minlength="2" maxlength="80" placeholder="Informe o seu nome de usuário:">
-                    
-                    <label for="nomeCompleto">NOME COMPLETO:</label>
-                    <input type="text" name="nomeCompleto" id="nomeCompleto">
-
-                    <label for="endereco">ENDEREÇO:</label>
-                    <input type="text" name="endereco" id="endereco">
-
-                    <label for="telefone">TELEFONE:</label>
-                    <input type="tel" name="telefone" id="telefone">
-
-                    <label for="email">EMAIL:</label>
-                    <input type="email" name="email" id="email">
-
+                    <input type="text" name="usuario_cad" id="usuario_cad" size="60" minlength="2" maxlength="80" placeholder="Informe o seu nome de usuário:" required>
                     <label for="senha_cad" id="etiqueta2">SENHA:</label>
-                    <input type="password" name="senha_cad" id="senha_cad" size="60" minlength="8" maxlength="80" placeholder="Digite a sua senha corretamente:">
+                    <input type="password" name="senha_cad" id="senha_cad" size="60" minlength="8" maxlength="80" placeholder="Digite a sua senha corretamente:" required>
+                    <label for="email" id="etiqueta3">E-MAIL:</label>
+                    <input type="email" name="email" id="email" placeholder="Informe o seu endereço de e-mail:" required>
+                    <label for="nomeCompleto" id="etiqueta4">NOME:</label>
+                    <input type="text" name="nomeCompleto" id="nomeCompleto" size="60" minlength="4" maxlength="80" placeholder="Informe o seu nome completo:" required>
+                </fieldset>
+                <fieldset class="campo">
+                    <legend>Logradouro</legend>
+                    <label for="endereco" id="etiqueta5">ENDEREÇO:</label>
+                    <input type="text" name="endereco" id="endereco" size="60" placeholder="Rua, Avenida, Esquina, Travessa" required>
+                    <label for="telefone" id="etiqueta6">TELEFONE:</label>
+                    <input type="tel" name="telefone" id="telefone" size="60" placeholder="Informe o seu número de telefone" required>
                 </fieldset>
                 <input type="submit" name="entrar" value="CADASTRAR" id="form-button">
             </form>
