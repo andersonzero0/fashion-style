d757b640e2003890a6ea38a4ac6ccf208bf92443
anderson <92057959+andersonzero0@users.noreply.github.com>
2022-10-08 13:49:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -24,18 +24,9 @@ $result = $conn->query($sql);
 </head>
 <body>
     <header>
-        <?php
-            if(isset($_SESSION['token_auth']) || isset($_SESSION['token_authAdmin'])){
-                echo 1;
-            }else{
-                echo 0;
-            }
-        ?>
-        <a href="controller/exit.php">exit</a>
-        <a href="view/login.php">login</a>
-        <a href="view/cadastrar.php">cadastrar</a>
-    </header>
 
+    </header>
+    
     <main>
         <form action="index.php" method="get">
             <input type="search" name="search" id="search">
