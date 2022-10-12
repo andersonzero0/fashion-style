22789abee2df8963eb5ca55d36e67c49471d2351
gabriel da silva pires <mr.boostphoenix@gmail.com>
2022-10-11 21:04:16 -0300

    Atualização de Estilização!
    Esta atualização é relativa à correção de erro de largura do formulário do arquivo "cadastrar.php" e também em relação ao arquivo "index.php" e sua estilização parte 1

--- index.php
+++ index.php
@@ -15,46 +15,93 @@ $result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/index.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/index-responsividade.css" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <title>Document</title>
 </head>
 <body>
-    <header>
+    <nav class="nav-bar">
+        <div class="logo">
+            <a href="index.php" target="_self" rel="next">Fashion Style</a>
+        </div>
+<?php
+    if(empty($_SESSION['token_auth']) && empty($_SESSION['token_authAdmin'])){
+?>
+        <!--SE NÃO HÁ LOGIN-->
+        <div class="menu">
+            <ul>
+                <li class="li"><a href="view/login.php" target="_self" rel="next" id="botao">LOGIN</a></li>
+                <li class="li"><a href="view/cadastrar.php" target="_self" rel="next">CADASTRAR</a></li>
+            </ul>
+        </div>
+<?php
+    }else{
+        require "model/connect-db.php";
+        if(isset($_SESSION['token_auth'])) {
+            $user = $_SESSION['token_auth'];
+            $sql1 = "SELECT * FROM info_users INNER JOIN usuarios ON info_users.id = usuarios.id WHERE usuario = '$user'";
+        }elseif(isset($_SESSION['token_authAdmin'])){
+            $user = 'admin';
+            $sql1 = "SELECT * FROM info_users INNER JOIN usuarios ON info_users.id = usuarios.id WHERE usuario = '$user'";
+        }
 
-    </header>
-    
-    <main>
-        <form action="index.php" method="get">
+        $result1 = $conn->query($sql1);
+        $row1 = $result1->fetch_assoc();
+?>
+        <!--SE JA HÁ LOGIN-->
+        <div class="menu">
+            <ul>
+                <li class="li"><a href="view/meuspedidos.php" target="_self" rel="next">MEUS PEDIDOS</a></li>
+                <li class="li-user">
+                    <a href="javascript:void(0)" class="dropbtn"><img src="assets/img/icon-user.png" alt="Ícone de usuário" id="icon-user"></a>
+                    <div class="dropdown-container">
+                        <p class="id-username"><?=$row1['nomeCompleto']?></p>
+                        <p class="id-user-email"><?=$row1['email']?></p>
+                        <a href="controller/exit.php"><button>SAIR</button></a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+<?php
+    }
+?>
+    </nav>
+    <main class="main">
+        <form action="index.php" method="get" class="search-box">
+            <span class="material-symbols-outlined">
+                search
+            </span>
             <input type="search" name="search" id="search">
-            <input type="submit" value="procurar" name="procurar" id="procurar">
+            <input type="submit" value="PROCURAR" name="procurar" id="procurar">
         </form>
-
-        <div id="produtos">
+        <div class="produtos">
 <?php
             if ($result->num_rows > 0) {
                 while($row = $result->fetch_assoc()) {
 ?>
-            <div id="produto">
-                <div id="img">
+            <div class="produto">
+                <div class="img">
                     <img src="assets/img/uploads/<?=$row['caminhoIMG']?>" alt="imgoi">
                 </div>
 
-                <div id="nome-pro">
+                <div class="nome-produto">
                     <p><?=$row['nome']?></p>
                 </div>
 
-                <div id="valor-pro">
+                <div class="valor-produto">
                     <p><?=$row['valor']?></p>
                 </div>
                 <form action="controller/verif-pedido.php" method="post">
                     <!--ocultar esse elemento-->
                     <input type="text" value="<?=$row['id'];?>" name="id">
 
-                    <input id="comprar" type="submit" value="comprar" name="comprar">
+                    <input id="comprar" type="submit" value="COMPRAR" name="comprar">
                 </form>
         </div>
 <?php
