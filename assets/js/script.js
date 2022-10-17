caba59ce620ffc646c2124fcb5bb1fea0d15d195
gabriel da silva pires <mr.boostphoenix@gmail.com>
2022-10-16 21:39:40 -0300

    Atualização de Estilização Parte 1

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function mostrar() {
+    var elementoHTML = window.document.getElementsByClassName('form-edit')
+    elementoHTML.style.display = 'flex'
+}
+function desaparecer() {
+    var elementoHTML = window.document.getElementsByClassName('form-edit')
+    elementoHTML.style.display = 'none'
+}
